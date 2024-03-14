bf8c9546d7d372ae049c472fe37f0ec85cafe813
Cactuzss <61464118+Cactuzss@users.noreply.github.com>
2024-03-14 11:22:51 +0400

    Add files via upload

---
+++ Second Part/DestroyTables.sql
@@ -0,0 +1,14 @@
+USE _Institute_;
+
+DROP TABLE	grades, 
+			disdisciplines_lecturers, 
+			groups, 
+			students, 
+			disciplines,
+			lecturers,
+			buildings,
+			departaments,
+			specialities,
+			adresses,
+			faculies,
+			control_forms;
