9cd8c2abecb15a4908db4b8c3e9d9ccf45968145
Cactuzss <61464118+Cactuzss@users.noreply.github.com>
2024-03-17 13:36:12 +0400

    Update Fast_setup.sql

--- Fast_setup.sql
+++ Fast_setup.sql
@@ -27,7 +27,8 @@ CREATE TABLE buildings(
 	faculty_id		INTEGER FOREIGN KEY REFERENCES faculties(id),
 	director_name	VARCHAR(64),
 	contact_mail	VARCHAR(64),
-	contact_phone	VARCHAR(16)
+	contact_phone	VARCHAR(16),
+	adress_id	INTEGER FOREIGN KEY REFERENCES adresses(id),
 );
 
 CREATE TABLE specialities(
