e0a326752b5cfbfa33ad38735961b06ccc21b5bf
Cactuzss <61464118+Cactuzss@users.noreply.github.com>
2024-03-17 13:41:47 +0400

    Update 4_Fill_buildings.sql
    Forgor to fill adresses

--- Second Part/Fill Tables/4_Fill_buildings.sql
+++ Second Part/Fill Tables/4_Fill_buildings.sql
@@ -1,25 +1,28 @@
 USE _Institute_;
 
-INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone)
+INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone, adress_id)
 	VALUES(
 		(SELECT id FROM faculties WHERE faculties.title = 'Факультет информатики'),
 		'Петров Иван Петрович',
 		'pevan@sobaka.mail',
-		'+89012421232'
+		'+89012421232',
+      	(SELECT id FROM adresses WHERE adresses.street = 'Улица Карла CJ')
 	);
 	
-INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone)
+INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone, adress_id)
 	VALUES(
 		(SELECT id FROM faculties WHERE faculties.title = 'Факультет искусств'),
 		'Пандов Вячеслав Станиславович',
 		'pandusvladislav@pochta.kva',
-		'+28421404012'
+		'+28421404012',
+      	(SELECT id FROM adresses WHERE adresses.street = 'Вгунгавская')
 	);
 	
-INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone)
+INSERT INTO buildings(faculty_id, director_name, contact_mail, contact_phone, adress_id)
 	VALUES(
 		(SELECT id FROM faculties WHERE faculties.title = 'Факультет экономики'),
 		'Фамилия Ольга Константиновна',
 		'kolyailitolya@chto.to',
-		'+190212449202'
+		'+190212449202',
+      	(SELECT id FROM adresses WHERE adresses.street = 'Дом')
 	);
