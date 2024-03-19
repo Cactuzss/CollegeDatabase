c8c878fa4aa1f8fa57c1cb7d6001f8a0c35300fb
Cactuzss <61464118+Cactuzss@users.noreply.github.com>
2024-03-19 12:39:07 +0400

    Delete Fifth Part directory

--- Fifth Part/GetAllGrades.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE GetGrades AS
-BEGIN
-	SELECT students.full_name, disciplines.title, result FROM grades
-    	JOIN students on students.id = student_id
-        JOIN disciplines ON disciplines.id = discipline_id
-END
