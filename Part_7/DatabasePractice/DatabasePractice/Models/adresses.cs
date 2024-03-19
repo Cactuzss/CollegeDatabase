6b9635a182ae06cabfc079fe0f99532269a6cb7a
Cactuzss <61464118+Cactuzss@users.noreply.github.com>
2024-03-19 12:42:05 +0400

    Started part 7

---
+++ Part_7/DatabasePractice/DatabasePractice/Models/adresses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabasePractice.Models
+{
+    public class adresses
+    {
+        public int id { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string street { get; set; }
+        public string house_number { get; set; }
+    }
+}
---
+++ Part_7/DatabasePractice/DatabasePractice/Models/students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasePractice.Models
+{
+    public class students
+    {
+        public int id { get; set; }
+        public string full_name { get; set; }
+        public int faculty_id { get; set; }
+        public int speciality_id { get; set; }
+        public DateTime birth_date { get; set; }
+        public string contact_mail { get; set; }
+        public string contact_phone { get; set; }
+    }
+}
